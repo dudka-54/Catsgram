c364b89c2bbda53abfcc87771ed29822e2eb5300
dudka-54 <kovboimolodoi@yandex.ru>
2026-02-16 16:07:44 +0400

    Первый коммит: добавлен проект Catsgram

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ConditionsNotMetException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.catsgram.controller;
+
+public class ConditionsNotMetException extends RuntimeException {
+    public ConditionsNotMetException(String message) {
+        super(message);
+    }
+}
